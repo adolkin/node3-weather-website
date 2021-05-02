667198cff6ebb68b04d1770615ea9bd511b1857a
Manh Tuan <tuanbk189@gmail.com>
2021-05-02 08:17:01 +0700

    49. Advanced Templating

--- src/app.js
+++ src/app.js
@@ -1,15 +1,18 @@
-const path = require('path');
-const express = require('express');
+const path = require('path')
+const express = require('express')
+const hbs = require('hbs')
 
-const app = express();
+const app = express()
 
 // Define paths for Express config
-const publicDirectoryPath = path.join(__dirname, '../public');
-const viewsPath = path.join(__dirname, '../templates');
+const publicDirectoryPath = path.join(__dirname, '../public')
+const viewsPath = path.join(__dirname, '../templates/views')
+const partialsPath = path.join(__dirname, '../templates/partials')
 
 // Setup handlebars engine and views location
-app.set('view engine', 'hbs');
+app.set('view engine', 'hbs')
 app.set('views', viewsPath)
+hbs.registerPartials(partialsPath)
 
 // Setup static directory to serve
 app.use(express.static(publicDirectoryPath))
@@ -17,21 +20,23 @@ app.use(express.static(publicDirectoryPath))
 app.get('', (req, res) => {
   res.render('index', {
     title: 'Weather',
-    name: 'Andrew Mead'
-  });
+    name: 'Tuan Nguyen'
+  })
 })
 
 app.get('/about', (req, res) => {
   res.render('about', {
     title: 'About Me',
-    name: 'Andrew Mead'
-  });
+    name: 'Tuan Nguyen'
+  })
 })
 
 app.get('/help', (req, res) => {
   res.render('help', {
-    helpText: 'This is some helpful text'
-  });
+    helpText: 'This is some helpful text',
+    title: 'Help',
+    name: 'Tuan Nguyen'
+  })
 })
 
 app.get('/weather', (req, res) => {
