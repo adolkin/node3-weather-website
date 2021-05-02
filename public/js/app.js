af54d5263662271b8c78f3624c641d93cc97e9cc
Manh Tuan <tuanbk189@gmail.com>
2021-05-02 15:15:44 +0700

    58. Creating a Search Form

--- public/js/app.js
+++ public/js/app.js
@@ -1,18 +1,22 @@
 console.log('Client side javascript file is loaded!')
 
-fetch('http://puzzle.mead.io/puzzle').then((response) => {
-  response.json().then((data) => {
-    console.log(data)
-  })
-})
+const weatherForm = document.querySelector('form')
+const search = document.querySelector('input')
 
-fetch('http://localhost:3000/weather?address=boston').then((data) => {
-  data.json().then((data) => {
-    if (data.error) {
-      console.log(data.error)
-    } else {
-      console.log(data.location)
-      console.log(data.forecast)
-    }
+weatherForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  const location = search.value
+  console.log(location);
+
+  fetch('http://localhost:3000/weather?address=' + location).then((data) => {
+    data.json().then((data) => {
+      if (data.error) {
+        console.log(data.error)
+      } else {
+        console.log(data.location)
+        console.log(data.forecast)
+      }
+    })
   })
 })
