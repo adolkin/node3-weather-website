3f7c1becb2a943899529765e7a11d9df44dd53d8
Manh Tuan <tuanbk189@gmail.com>
2021-05-03 08:45:04 +0700

    67. Deploying Node.js to Heroku

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,3 @@
-console.log('Client side javascript file is loaded!')
-
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
 const messageOne = document.querySelector('#message-1')
@@ -13,7 +11,7 @@ weatherForm.addEventListener('submit', (e) => {
   messageOne.textContent = 'Loading...'
   messageTwo.textContent = ''
 
-  fetch('http://localhost:3000/weather?address=' + location).then((data) => {
+  fetch('/weather?address=' + location).then((data) => {
     data.json().then((data) => {
       if (data.error) {
         messageOne.textContent = data.error
